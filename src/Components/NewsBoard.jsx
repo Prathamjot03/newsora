63d7dc25bdea0a848c7b75cf2b6410d22e0af967
Prathamjot <prathamjotsingh03@gmail.com>
2026-01-16 22:47:24 +0530

    apiKey

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
+const apiKey = "8b624391d0a845eca2394838457bfe5c";
 const NewsBoard = ({category}) => {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
         fetch(url).then(response => response.json()).then(data => setArticles(data.articles));
     }, [category]);
 
